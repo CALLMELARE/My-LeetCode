6a964fbb5a12f9923224029706d52f72f687d63e
左云飞 <yunfei@tju.edu.cn>
2023-01-06 14:53:08 +0800

    update

---
+++ 709.转换成小写字母.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=709 lang=cpp
+ *
+ * [709] 转换成小写字母
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string toLowerCase(string s)
+    {
+        for (char &c : s)
+        {
+            c = tolower(c);
+        }
+        return s;
+    }
+};
+// @lc code=end
