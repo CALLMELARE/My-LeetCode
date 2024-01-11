48e98da6c0e4f7f7c4e90eef56dc5973bdcfc1d9
LARE <zuoyun-135@163.com>
2024-01-11 22:27:24 +0800

    Create 560.和为-k-的子数组.js

---
+++ JavaScript/560.和为-k-的子数组.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=javascript
+ *
+ * [560] 和为 K 的子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  let count = 0;
+  let sum = 0;
+  const map = new Map();
+  map.set(0, 1);
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+    if (map.has(sum)) {
+      map.set(sum, map.get(sum) + 1);
+    } else {
+      map.set(sum, 1);
+    }
+  }
+
+  return count;
+};
+// @lc code=end
