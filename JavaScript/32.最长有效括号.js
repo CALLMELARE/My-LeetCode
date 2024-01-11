e69b260cb45a71453d5512db1579d16add1054c4
LARE <yunfei@tju.edu.cn>
2024-01-11 13:20:12 +0800

    JavaScript/32.最长有效括号.js

---
+++ JavaScript/32.最长有效括号.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=javascript
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function (s) {
+  let maxLen = 0;
+  let stack = [-1];
+  let len = s.length;
+
+  for (let i = 0; i < len; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else {
+      stack.pop();
+      if (stack.length === 0) {
+        stack.push(i);
+      } else {
+        maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+      }
+    }
+  }
+
+  return maxLen;
+};
+// @lc code=end
