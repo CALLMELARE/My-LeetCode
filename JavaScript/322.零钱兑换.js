d2459b07fec49f8877f53602706409abcf7baa44
左云飞 <yunfei@tju.edu.cn>
2024-01-13 17:59:26 +0800

    JavaScript/322.零钱兑换.js

---
+++ JavaScript/322.零钱兑换.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=javascript
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
+  dp[0] = 0;
+
+  for (let i = 1; i <= coins.length; i++) {
+    for (let j = coins[i - 1]; j <= amount; j++) {
+      if (dp[j] - 1 > dp[j - coins[i - 1]]) {
+        dp[j] = 1 + dp[j - coins[i - 1]];
+      }
+    }
+  }
+
+  return dp[amount] === Number.MAX_SAFE_INTEGER ? -1 : dp[amount];
+};
+// @lc code=end
