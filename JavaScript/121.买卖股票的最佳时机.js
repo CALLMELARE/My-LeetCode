cfaf454e7426797b024d9b9cdc3018414540f260
左云飞 <yunfei@tju.edu.cn>
2024-01-13 18:11:10 +0800

    JavaScript/121.买卖股票的最佳时机.js

---
+++ JavaScript/121.买卖股票的最佳时机.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=javascript
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+  }
+
+  return maxProfit;
+};
+// @lc code=end
