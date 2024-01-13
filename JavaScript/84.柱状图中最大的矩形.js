5a521591f798fbab6977b8876e45323945d3a620
左云飞 <yunfei@tju.edu.cn>
2024-01-13 23:44:05 +0800

    JavaScript/84.柱状图中最大的矩形.js

---
+++ JavaScript/84.柱状图中最大的矩形.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=javascript
+ *
+ * [84] 柱状图中最大的矩形
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+  const stack = [];
+  let maxArea = 0;
+
+  for (let i = 0; i <= heights.length; i++) {
+    const currHeight = i === heights.length ? 0 : heights[i];
+
+    while (stack.length > 0 && currHeight < heights[stack[stack.length - 1]]) {
+      const height = heights[stack.pop()];
+      const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+      maxArea = Math.max(maxArea, height * width);
+    }
+
+    stack.push(i);
+  }
+
+  return maxArea;
+};
+// @lc code=end
