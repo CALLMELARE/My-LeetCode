1552c0c76237af71c89fffac17d55d95b9717de6
左云飞 <yunfei@tju.edu.cn>
2024-01-14 16:19:24 +0800

    JavaScript/231.2-的幂.js

---
+++ JavaScript/231.2-的幂.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=javascript
+ *
+ * [231] 2 的幂
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  if (n <= 0) return false;
+  while (n >= 1) {
+    if (n !== 1 && n % 2 !== 0) return false;
+    n /= 2;
+  }
+  return true;
+};
+// @lc code=end
