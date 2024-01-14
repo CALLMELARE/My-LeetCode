d12872ca95533936726c1d6ff4864e1d3382a18a
左云飞 <yunfei@tju.edu.cn>
2024-01-14 16:41:11 +0800

    JavaScript/1281.整数的各位积和之差.js

---
+++ JavaScript/1281.整数的各位积和之差.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1281 lang=javascript
+ *
+ * [1281] 整数的各位积和之差
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+
+};
+// @lc code=end
+
