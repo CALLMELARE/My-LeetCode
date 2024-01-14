7a332a2b3c93f9560b1181f196f89122d48ece10
左云飞 <yunfei@tju.edu.cn>
2024-01-14 17:02:34 +0800

    JavaScript/852.山脉数组的峰顶索引.js

---
+++ JavaScript/852.山脉数组的峰顶索引.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=852 lang=javascript
+ *
+ * [852] 山脉数组的峰顶索引
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] < arr[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};
+// @lc code=end
