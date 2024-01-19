ae53ead1bdca768929a02b0ae090a98806629800
左云飞 <yunfei@tju.edu.cn>
2024-01-19 19:12:56 +0800

    JavaScript/622.设计循环队列.js

---
+++ JavaScript/622.设计循环队列.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=622 lang=javascript
+ *
+ * [622] 设计循环队列
+ */
+
+// @lc code=start
+/**
+ * @param {number} k
+ */
+var MyCircularQueue = function (k) {
+  this.queue = new Array(k);
+  this.front = 0;
+  this.rear = 0;
+  this.size = 0;
+  this.capacity = k;
+};
+
+/**
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function (value) {
+  if (this.isFull()) return false;
+  this.queue[this.rear] = value;
+  this.rear = (this.rear + 1) % this.capacity;
+  this.size++;
+  return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function () {
+  if (this.isEmpty()) return false;
+  this.front = (this.front + 1) % this.capacity;
+  this.size--;
+  return true;
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function () {
+  if (this.isEmpty()) return -1;
+  return this.queue[this.front];
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function () {
+  if (this.isEmpty()) return -1;
+  return this.queue[(this.rear - 1 + this.capacity) % this.capacity];
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function () {
+  return this.size === 0;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function () {
+  return this.size === this.capacity;
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
+// @lc code=end
