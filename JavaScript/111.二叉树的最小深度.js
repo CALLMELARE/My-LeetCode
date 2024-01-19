2f5eaf644d144a837d51bdc161135c2b1b20f1b7
左云飞 <yunfei@tju.edu.cn>
2024-01-19 19:52:45 +0800

    JavaScript/111.二叉树的最小深度.js

---
+++ JavaScript/111.二叉树的最小深度.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=javascript
+ *
+ * [111] 二叉树的最小深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+  if (root === null) return 0;
+  if (root.left === null && root.right === null) return 1;
+  let min = Infinity;
+  if (root.left !== null) min = Math.min(min, minDepth(root.left));
+  if (root.right !== null) min = Math.min(min, minDepth(root.right));
+  return min + 1;
+};
+// @lc code=end
