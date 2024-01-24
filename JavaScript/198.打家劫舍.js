151600e07889707726c60eb53b480da83ee4a295
左云飞 <yunfei@tju.edu.cn>
2024-01-24 11:34:08 +0800

    JavaScript/198.打家劫舍.js

--- JavaScript/198.打家劫舍.js
+++ JavaScript/198.打家劫舍.js
@@ -9,15 +9,10 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i])
 var rob = function (nums) {
   const n = nums.length;
-  if (n === 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return nums[0];
-  }
-
   const dp = new Array(n).fill(0);
   dp[0] = nums[0];
   dp[1] = Math.max(nums[0], nums[1]);
