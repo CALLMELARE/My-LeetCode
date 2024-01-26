50979065ba6bcf548b9eb9af7e5c976c112342c8
左云飞 <yunfei@tju.edu.cn>
2024-01-26 16:19:20 +0800

    JavaScript/322.零钱兑换.js

--- JavaScript/322.零钱兑换.js
+++ JavaScript/322.零钱兑换.js
@@ -10,15 +10,15 @@
  * @param {number} amount
  * @return {number}
  */
+
+// dp[j] = Math.min(dp[j], dp[j - coins[i - 1]] + 1);
 var coinChange = function (coins, amount) {
   const dp = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
   dp[0] = 0;
 
   for (let i = 1; i <= coins.length; i++) {
     for (let j = coins[i - 1]; j <= amount; j++) {
-      if (dp[j] - 1 > dp[j - coins[i - 1]]) {
-        dp[j] = 1 + dp[j - coins[i - 1]];
-      }
+      dp[j] = Math.min(dp[j], dp[j - coins[i - 1]] + 1);
     }
   }
 
